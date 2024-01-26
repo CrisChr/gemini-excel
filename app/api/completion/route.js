d3733ab051a41a37cf3473e1a2fb051b4f608d6a
CrisChr <chr7749@hotmail.com>
2024-01-26 15:32:14 +0800

    remove try-catch

--- app/api/completion/route.js
+++ app/api/completion/route.js
@@ -1,42 +1,40 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleGenerativeAIStream, StreamingTextResponse } from "ai";
 import { NextResponse } from "next/server";
 
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY || '');
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY || "");
 
 // Set the runtime to edge
 export const runtime = "edge";
 
 export async function POST(req) {
-  try {
-    const { language, prompt } = await req.json();
+  const { language, prompt } = await req.json();
 
-    // Ask Google Generative AI for a streaming completion given the prompt
-    const response = await genAI
-    .getGenerativeModel({ model: 'gemini-pro' })
+  // Ask Google Generative AI for a streaming completion given the prompt
+  const response = await genAI
+    .getGenerativeModel({ model: "gemini-pro" })
     .generateContentStream({
-      contents: [{ role: 'user', parts: [{ text: `${prompt}. ${process.env.LANGUAGE_TIP} ${language}. ${process.env.THANK_YOU}` }] }],
+      contents: [
+        {
+          role: "user",
+          parts: [
+            {
+              text: `${prompt}. ${process.env.LANGUAGE_TIP} ${language}. ${process.env.THANK_YOU}`,
+            },
+          ],
+        },
+      ],
     });
 
-    console.log("response: ", response);
+  console.log("response: ", response);
 
-    // Convert the response into a friendly text-stream
-    const stream = GoogleGenerativeAIStream(response);
+  // Convert the response into a friendly text-stream
+  const stream = GoogleGenerativeAIStream(response);
 
-    // Respond with the stream
-    return new StreamingTextResponse(stream);
-
-  } catch (error) {
-    // Check if the error is an APIError
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
-    } else {
-      throw error;
-    }
-  }
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 }
 
 /**
- * 
+ *
  */
